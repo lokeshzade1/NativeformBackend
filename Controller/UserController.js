fb34704688a88a98b6e421ad8f23fed0be70a219
lokeshzade1 <zadelokesh1@gmail.com>
2023-07-10 16:28:26 +0530

    new from lokesh

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -19,4 +19,22 @@ const registerUser = async (req, res) => {
     });
   }
 };
+const registerUserFind = async (req, res) => {
+  console.log(user);
+  try {
+    let result = await user.find({
+      name: req.body.name,
+      password: req.body.password,
+    });
+
+    res.status(200).json({
+      value: 'success',
+      result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+    });
+  }
+};
 module.exports = { registerUser };
