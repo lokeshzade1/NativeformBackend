6f0408def900e3d29df3c1444d0298ded6c22793
lokeshzade1 <zadelokesh1@gmail.com>
2023-09-24 14:40:13 +0530

    new updated

--- Routes/UserInstalogin.js
+++ Routes/UserInstalogin.js
@@ -1,9 +1,9 @@
-let { InstaLogin } = require('../Controller/InstaLogin');
+let { loginUser } = require('../Controller/Userlogincontrolller');
 let express = require('express');
 
 let routes = express();
 
-routes.post('/registerInstagramlogin', InstaLogin);
+routes.post('/registerInstagramlogin', loginUser);
 // routes.post('/findRegisterUser', registerUserFind);
 // routes.delete('/deleteAllRegisterUser', deleteRegisterUser);
 //   routes.get('/getRegisterInstagramUser', getRegisterInstagramUser);
